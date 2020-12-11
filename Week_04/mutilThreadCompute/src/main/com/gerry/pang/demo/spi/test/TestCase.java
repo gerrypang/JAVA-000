8bdb095f48709aa00e773e1be20c5dff693ca1ce
pangguowei <pangguowei@yxqiche.com>
2020-12-11 10:28:54 +0800

    Add 参考资料链接

--- Week_04/mutilThreadCompute/src/main/com/gerry/pang/demo/spi/test/TestCase.java
+++ Week_04/mutilThreadCompute/src/main/com/gerry/pang/demo/spi/test/TestCase.java
@@ -8,6 +8,7 @@ import com.gerry.pang.demo.spi.SearchDemoSPI;
 public class TestCase {
 
 	public static void main(String[] args) {
+		// 参考资料： https://zhuanlan.zhihu.com/p/28909673
 		ServiceLoader<SearchDemoSPI> s = ServiceLoader.load(SearchDemoSPI.class);
 		Iterator<SearchDemoSPI> iterator = s.iterator();
 		while (iterator.hasNext()) {
